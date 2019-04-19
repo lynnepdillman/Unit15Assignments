cf1f260de2de5e0f87e3ef88f69190aaf82a33c2
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-04-19 09:27:33 -0700

    Update Locatable.java

---
+++ src/Unit15Assignmnets/Locatable.java
@@ -0,0 +1,15 @@
+package Unit15Assignmnets;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public interface Locatable
+{
+    public void setPos( int x, int y);
+    public void setxPos( int x );
+    public void setyPos( int y );
+
+    public int getxPos();
+    public int getyPos();
+}
